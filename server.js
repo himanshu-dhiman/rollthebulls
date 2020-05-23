0cba25584b9c6c698e8fff4b036501720ffc021d
Gokata1 <shrey1dhyani@gmail.com>
2020-05-23 12:48:15 -0700

    Just testing :p

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Why is there no next???
 app.get("/", (req, res) => res.send("Hello Bulls!"));
 
 app.listen(port, () =>
